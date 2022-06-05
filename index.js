4990487d3ab8417b8de4cacf58bc280ccce50f65
kkauma <kylekauma@gmail.com>
2022-06-05 17:48:08 -0600

    add commentary for various functions, clean up extra code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Generate text of 15 random letters
 function makeid() {
   let text = "";
   let possible =
@@ -9,16 +10,16 @@ function makeid() {
   return text;
 }
 
+// Select all of the buttons to generate passwords for
 let btn1 = document.querySelector("#password-one");
 let btn2 = document.querySelector("#password-two");
 let btn3 = document.querySelector("#password-three");
 let btn4 = document.querySelector("#password-four");
 
+// Input the random text into each button
 function generatePasswords() {
   btn1.textContent = makeid();
   btn2.textContent = makeid();
   btn3.textContent = makeid();
   btn4.textContent = makeid();
 }
-
-// console.log(makeid());
